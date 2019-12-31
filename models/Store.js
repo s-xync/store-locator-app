432cfb6873af988119d0c87dd99a0fec57cdd16c
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-01-01 01:30:52 +0530

    geocoding locations in presave mongoose hook

--- models/Store.js
+++ models/Store.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const geocoder = require("../utils/geocoder");
 
 const StoreSchema = new mongoose.Schema({
   storeId: {
@@ -27,6 +28,21 @@ const StoreSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// Geocode & create location
+StoreSchema.pre("save", async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress
+  };
+
+  // Do not  save address
+  this.address = undefined;
+
+  next();
 });
 
 module.exports = mongoose.model("Store", StoreSchema);
