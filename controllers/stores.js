f25a325055511e6b425778c37c270919c8a5c8a8
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-01-01 01:10:21 +0530

    api to get stores and create stores

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,6 +1,45 @@
+const HttpStatus = require("http-status-codes");
+const Store = require("../models/Store");
+
 // @desc get all stores
 // @route GET /api/v1/stores
 // @acess public
-exports.getStores = (req, res, next) => {
-  res.send("hello");
+exports.getStores = async (req, res, next) => {
+  try {
+    const stores = await Store.find();
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      count: stores.length,
+      data: stores
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal server error." });
+  }
+};
+
+// @desc create a store
+// @route POST /api/v1/stores
+// @acess public
+exports.addStore = async (req, res, next) => {
+  try {
+    const store = await Store.create(req.body);
+
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      data: store
+    });
+  } catch (err) {
+    console.error(err);
+    if (err.code === 11000) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ error: "Store already exists." });
+    }
+    res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal server error." });
+  }
 };
